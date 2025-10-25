d12c1d4caec2c0394c092f5e2e256bb3a47e6d41
Ayodeji Akenroye <e.manuel_07@yahoo.com>
2025-10-25 10:59:50 +0100

    build: remove support for  an optional dependency(google calendar) - v0.1.0a3

--- django_affiliate_system/__version__.py
+++ django_affiliate_system/__version__.py
@@ -1,6 +1,6 @@
 """Version information for django-affiliate-system."""
 
-__version__ = "0.1.0a2"
+__version__ = "0.1.0a3"
 __title__ = "django-affiliate-system"
 __description__ = "A comprehensive Django affiliate marketing and referral tracking system"
 __author__ = "Ayodeji Akenroye"
