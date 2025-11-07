76899acb0a096295759b943fb508b8f7d06e667c
Ayodeji Akenroye <e.manuel_07@yahoo.com>
2025-11-07 21:20:05 +0100

    chore: remove duplicate version declaration from pyproject.toml

--- django_affiliate_system/__version__.py
+++ django_affiliate_system/__version__.py
@@ -1,6 +1,6 @@
 """Version information for django-affiliate-system."""
 
-__version__ = "0.1.0a5"
+__version__ = "0.1.0a6"
 __title__ = "django-affiliate-system"
 __description__ = "A comprehensive Django affiliate marketing and referral tracking system"
 __author__ = "Ayodeji Akenroye"
